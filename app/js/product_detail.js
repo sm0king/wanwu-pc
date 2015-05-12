22ac734e8b270f8393472e97be5f3333f3ab61db
sm0king <lntianda@gmail.com>
2015-05-12 16:24:04 +0800

    修改商品详情 热门标签页

--- app/js/product_detail.js
+++ app/js/product_detail.js
@@ -1,6 +1,22 @@
 $(function(){ 
-    var ue = UE.getEditor('productDetail'); 
+    var ue = UE.getEditor('productDetail');
+    $('#tagsinput').tagsInput();
     $('#flags').delegate('button','click', function(e){
-        $(e.currentTarget).parent().toggleClass('selected');
+        //获取tags内容
+        var val = $(this).text();
+        //检查该tags是否存在
+        if ($('#tagsinput').tagExist(val)) {
+            //.1 存在 删除，删除css
+            $('#tagsinput').removeTag(val);
+            $(e.currentTarget).parent().removeClass('selected');
+            // debugger;
+        }else{
+            //.2 不存在，添加，增加css
+            $('#tagsinput').addTag(val);
+            $(e.currentTarget).parent().addClass('selected');
+            // debugger;
+        }
+        //debugger;
+        //$(e.currentTarget).parent().toggleClass('selected');
     });
 });
