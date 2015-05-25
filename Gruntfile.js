b5255c08a6ad7ec8cebba2c59d268a0bccf3ffe2
sm0king <lntianda@gmail.com>
2015-05-25 10:46:24 +0800

    修改master的一些东西

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,7 +67,7 @@ module.exports = function (grunt) {
         },
         watch:{
             jade:{
-                tasks: [ 'jade' ],
+                tasks: [ 'jade:compile' ],
                 files: [
                     'template/**/*.jade',
                     'template/**/*.html'
@@ -78,25 +78,64 @@ module.exports = function (grunt) {
             }
         },
         jade: {
-            options:{
+            compile:{
+                options:{
+                    basedir:'template',
+                    pretty:true
+                },
+                files:[{
+                    expand:true,
+                    src:[
+                    'template/**/*.jade',
+                    'template/**/*.html'],
+                    dest:'app/'
+                }]
+            }
+            /*options:{
                 pretty: true
             },
             analysis:{
                 files:jadeTamplates
-            }
+            }*/
         }
     });
+    var JadeInheritance = require('jade-inheritance');
+    var changedFiles = [];
+
+    var onChange = grunt.util._.debounce(function() {
+        var options = grunt.config('jade.compile.options');
+        var dependantFiles = [];
+
+        changedFiles.forEach(function(filename) {
+          var directory = options.basedir;
+          var inheritance = new JadeInheritance(filename, directory, options);
+          dependantFiles = dependantFiles.concat(inheritance.files);
+        });
+
+        var config = grunt.config('jade.compile.files')[0];
+        config.src = dependantFiles;
+        grunt.config('jade.compile.files', [config]);
+
+        changedFiles = [];
+    }, 200)
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('jade-inheritance');
 
     grunt.event.on('watch', function(action, filepath){
+        changedFiles.push(filepath);
+        onChange();
+        /*console.log('xxxxxxxxxxxxxxxxxxxxxx');
+        jade -p <filepath>
+        // console.log(jade -p <filepath>)
+        console.log('xxxxxxxxxxxxxxxxxxxxxx');
         var target = filepath.replace('template', 'app'),
             config = {};
 
         config[ target ] = filepath;
-        grunt.config('jade.analysis.files', config);
+        grunt.config('jade.analysis.files', config);*/
     });
 
     grunt.registerTask('server', ['jade','connect:demo', 'watch']);
